b35ed4ebad6b7a77eed0ca0a7887b761c65a8846
Alex Camacho <alsbrain@optonline.net>
2020-11-07 18:40:18 -0800

    refactoring, fixed mobile view

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-var savedCounter = 1
-
+// Stores the value for how many cities have been searched.
+var cityCounter = 1
+init()
 $('#search-form').on('submit', function (event) {
     event.preventDefault()
     var userInput = $('#search-box').val()
     getWeather(userInput)
-    createCityButton(userInput)
-    savedCounter++
+    addCity(userInput)
+    cityCounter++
+    createLocalStorage(userInput)
     $('#search-box').val('')
 })
 
@@ -15,17 +17,33 @@ $('[id^=userCity]').on('click', function (event) {
     event.preventDefault()
     var city = $(this)[0].innerText
     getWeather(city)
+    createLocalStorage(city)
 })
-function createCityButton(userInput) {
-    if(savedCounter >= 10){
-        savedCounter = 1
+
+// Adds the most recently searched city to the bottom of the list. When the last text field is reached, reset to the first and overwrite from there.
+function addCity(userInput) {
+    if(cityCounter >= 10){
+        cityCounter = 1
     }
-    $(`#userCity${savedCounter}`).text(`${userInput}`)
+    $(`#userCity${cityCounter}`).text(`${userInput}`)
 
 }
 
+
+// Initiliazes
+function init (){
+    if(localStorage.getItem('Last City')){
+        getWeather(localStorage.getItem('Last City'))
+    }
+}
+
+// Sets the local storage for the city that is passed in
+function createLocalStorage(city) {
+    localStorage.setItem('Last City', city)
+}
+
+// Calls the OpenWeatherMap 5 Day/3 Hour Forecast API to generate the output for the weather 
 function getWeather(userInput) {
-    // console.log(userInput)
 
     var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=52f80a8e6eee61dc210e113236e5b264`
 
@@ -34,8 +52,6 @@ function getWeather(userInput) {
         method: 'GET'
     }).then(function (response) {
 
-        // console.log(response.list)
-        //    var date = response.list[0].dt_txt
         // Counter controls where the information will be stored
         var counter = 0
 
@@ -43,33 +59,48 @@ function getWeather(userInput) {
         for (var i = 3; i < response.list.length; i += 8) {
 
             var temp = response.list[i].main.temp
+            // Convert temp from Kelvin to Fahrenheit 
             temp = Math.floor(((temp - 273.15) * 1.8) + 32)
             var humidity = response.list[i].main.humidity
             var condition = response.list[i].weather[0].main
+
+            // Grab the date information from the response, then convert it to display the date in MM/DD/YYYY format.
             var cardDate = new Date(response.list[i].dt_txt)
             cardDate = cardDate.toLocaleDateString()
-            // cardDate = 
+
+            // Call the checkConditions function to determine which icon to display on the card
             condition = checkConditions(condition)
 
-            var weatherCard = $(`#day${counter + 1}`)
+            // var weatherCard = $(`#day${counter + 1}`)
             // Clear the card before adding new information
-            weatherCard.html('')
-            var weatherInfo = $(`<div class= 'card-body'>${cardDate}<br>Temp: ${temp}&#8457<br>Humidity: ${humidity}%<br>${condition}</div>`)
-            $(weatherCard).append(weatherInfo)
+            // weatherCard.html('')
+            $(`#cardDate${counter + 1}`).html(cardDate)
+            $(`#condition${counter + 1}`).html(condition)
+            $(`#temp${counter + 1}`).html(`Temp: ${temp}&#8457`)
+            $(`#humid${counter + 1}`).html(`Humidity: ${humidity}%`)
+
+            // <div id="cardDate1"></div>
+            //                 <div id="condition1"></div>
+            //                 <div id="temp1"></div>
+            //                 <div id="humid1"></div>
+
+
+            // var weatherInfo = $(`<div class= 'card-body'>${cardDate}<br>${condition}<br>Temp: ${temp}&#8457<br>Humidity: ${humidity}%</div>`)
+            // $(weatherCard).append(weatherInfo)
 
             // Counter is incremented after data is stored
             counter++
         }
 
     })
-
+    // Call for the OpenWeatherMap Current Weather API
     var dayURL = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=52f80a8e6eee61dc210e113236e5b264`
 
     $.ajax({
         url: dayURL,
         method: 'GET'
     }).then(function (response) {
-        // console.log(response)
+        console.log(response)
         var temp = response.main.temp
         temp = Math.floor(((temp - 273.15) * 1.8) + 32)
         var humidity = response.main.humidity
@@ -94,12 +125,14 @@ function getWeather(userInput) {
         var latitude = response.coord.lat
 
         $.ajax({
+            // Call for the OpenWeatherMap UV Index API.
             url: `http://api.openweathermap.org/data/2.5/uvi?lat=${latitude}&lon=${longitude}&appid=52f80a8e6eee61dc210e113236e5b264`,
             method: 'GET'
         }).then(function (response) {
             var uvIndex = $('#uvIndex')
             uvIndex.html(`${response.value}`)
 
+            // Changes the color of the text field based on the value
             switch (true) {
                 case (response.value < 3 && response.value >= 0):
                     uvIndex.css('background-color', 'rgb(99, 204, 67)')
@@ -133,7 +166,9 @@ function checkConditions(condition) {
         case 'Snow':
             condition = '<i class="fas fa-snowflake"></i>'
             break
-        case 'Haze', 'Mist', 'Fog':
+        case 'Haze':
+        case 'Mist':
+        case 'Fog':
             condition = '<i class="fas fa-smog" style="color:grey"></i>'
             break
     }
