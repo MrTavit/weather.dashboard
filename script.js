b0cd0c495557454fc006e83dfa66889aca138294
Alex Camacho <alsbrain@optonline.net>
2020-11-07 16:45:01 -0800

    added search function to previous results

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+var savedCounter = 1
+
 $('#search-form').on('submit', function (event) {
     event.preventDefault()
-
     var userInput = $('#search-box').val()
-    console.log(userInput)
+    getWeather(userInput)
+    createCityButton(userInput)
+    savedCounter++
+    $('#search-box').val('')
+})
+
+
+// When any id is clicked that starts with the text 'userCity' run this function.
+$('[id^=userCity]').on('click', function (event) {
+    event.preventDefault()
+    var city = $(this)[0].innerText
+    getWeather(city)
+})
+function createCityButton(userInput) {
+    if(savedCounter >= 10){
+        savedCounter = 1
+    }
+    $(`#userCity${savedCounter}`).text(`${userInput}`)
+
+}
+
+function getWeather(userInput) {
+    // console.log(userInput)
 
     var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=52f80a8e6eee61dc210e113236e5b264`
 
@@ -11,32 +34,29 @@ $('#search-form').on('submit', function (event) {
         method: 'GET'
     }).then(function (response) {
 
-        console.log(response.list)
+        // console.log(response.list)
         //    var date = response.list[0].dt_txt
-
-
         // Counter controls where the information will be stored
         var counter = 0
 
         // Starts at the 4th result to display the noon forecast, jumps every 8 positions to get the noon forecast for each day.
-        for (var i = 4; i < response.list.length; i+= 8) {
+        for (var i = 3; i < response.list.length; i += 8) {
 
-                var temp = response.list[i].main.temp
-                temp = Math.floor(((temp - 273.15) * 1.8) + 32)
-                var humidity = response.list[i].main.humidity
-                var condition = response.list[i].weather[0].main
-                var cardDate = new Date(response.list[i].dt_txt)
-                cardDate = cardDate.toLocaleDateString()
-                // cardDate = 
+            var temp = response.list[i].main.temp
+            temp = Math.floor(((temp - 273.15) * 1.8) + 32)
+            var humidity = response.list[i].main.humidity
+            var condition = response.list[i].weather[0].main
+            var cardDate = new Date(response.list[i].dt_txt)
+            cardDate = cardDate.toLocaleDateString()
+            // cardDate = 
+            condition = checkConditions(condition)
 
-                condition = checkConditions(condition)
-    
-                var weatherCard = $(`#day${counter + 1}`)
-                // Clear the card before adding new information
-                weatherCard.html('')
-                var weatherInfo = $(`<div class= 'card-body'>${cardDate}<br>Temp: ${temp}&#8457<br>Humidity: ${humidity}%<br>${condition}</div>`)
-                $(weatherCard).append(weatherInfo)
-            
+            var weatherCard = $(`#day${counter + 1}`)
+            // Clear the card before adding new information
+            weatherCard.html('')
+            var weatherInfo = $(`<div class= 'card-body'>${cardDate}<br>Temp: ${temp}&#8457<br>Humidity: ${humidity}%<br>${condition}</div>`)
+            $(weatherCard).append(weatherInfo)
+
             // Counter is incremented after data is stored
             counter++
         }
@@ -49,14 +69,14 @@ $('#search-form').on('submit', function (event) {
         url: dayURL,
         method: 'GET'
     }).then(function (response) {
-        console.log(response)
+        // console.log(response)
         var temp = response.main.temp
         temp = Math.floor(((temp - 273.15) * 1.8) + 32)
         var humidity = response.main.humidity
         var windSpeed = response.wind.speed
         var city = response.name
         var condition = response.weather[0].main
-        console.log(condition)
+        // console.log(condition)
         condition = checkConditions(condition)
         var currentDate = new Date()
         currentDate = currentDate.toLocaleDateString()
@@ -78,29 +98,26 @@ $('#search-form').on('submit', function (event) {
             method: 'GET'
         }).then(function (response) {
             var uvIndex = $('#uvIndex')
-            uvIndex.html(` ${response.value}`)
+            uvIndex.html(`${response.value}`)
 
             switch (true) {
                 case (response.value < 3 && response.value >= 0):
-                    uvIndex.css('background-color', 'green');
-                    break;
+                    uvIndex.css('background-color', 'rgb(99, 204, 67)')
+                    break
                 case (response.value < 6 && response.value >= 3):
-                    uvIndex.css('background-color', 'rgb(231, 231, 45)');
-                    break;
+                    uvIndex.css('background-color', 'yellow')
+                    break
                 case (response.value < 8 && response.value >= 6):
-                    uvIndex.css('background-color', 'orange');
-                    break;
+                    uvIndex.css('background-color', 'orange')
+                    break
                 case (response.value >= 8):
-                    uvIndex.css('background-color', 'red');
-                    break;
+                    uvIndex.css('background-color', 'red')
+                    break
             }
         })
 
     })
-})
-
-
-
+}
 
 function checkConditions(condition) {
     switch (condition) {
@@ -108,13 +125,16 @@ function checkConditions(condition) {
             condition = '<i class="fas fa-cloud-sun"></i>'
             break
         case 'Clear':
-            condition = '<i class="fas fa-sun"></i>'
+            condition = '<i class="fas fa-sun" style="color:orange"></i>'
             break
         case 'Rain':
-            condition = '<i class="fas fa-cloud-rain"></i>'
+            condition = '<i class="fas fa-cloud-rain" style="color:lightgrey"></i>'
             break
         case 'Snow':
             condition = '<i class="fas fa-snowflake"></i>'
+            break
+        case 'Haze', 'Mist', 'Fog':
+            condition = '<i class="fas fa-smog" style="color:grey"></i>'
             break
     }
     return condition
